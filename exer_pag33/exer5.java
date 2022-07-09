af3b452072acc5918aaaba1c958a4adc8aa646fe
Isabele <104366065+iMendes111@users.noreply.github.com>
2022-07-09 01:09:02 -0300

    Initial commit

---
+++ exer_pag33/exer5.java
@@ -0,0 +1,33 @@
+package exer_pag33;
+
+import java.util.Scanner;
+
+public class exer5 {
+
+    static float Vhora = 10.25f;
+    static float Salario;
+
+        public static void main(String[] args) {
+
+            System.out.println("");
+            System.out.print(" Número de horas trabalhadas: ");
+            try (Scanner horas = new Scanner(System.in)) {
+                int i = horas.nextInt();
+
+                Salario = Vhora * i;
+                if (Salario < 50.00f) {
+                    System.out.println("");
+                    System.out.println(" Atenção, dirija-se à direção do Hotel!");
+                } else {
+            System.out.println("");
+            System.out.println(" O valor diário a ser recebido é de R$" + Salario);
+            System.out.println("");
+
+                }
+            }
+           
+                
+    }
+}
+    
+
